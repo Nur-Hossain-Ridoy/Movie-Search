84c2322f814b42a57344a50d7dfe0963a3630004
NH Ridoy <ridoydeveloper@gmail.com>
2022-11-01 18:23:19 +0600

    a simple react app which shows movies using api

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard'
+
+const API_URL = 'http://www.omdbapi.com?apikey=50c55108'
+
+const App = () => {
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchMovies = async (title) => {
+        const response  = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+        setMovies(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies('spiderman')
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>Movie Land</h1>
+            {/* search input and icon  */}
+            <div className="search">
+                <input 
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => { setSearchTerm(e.target.value)}}
+                    name="searchTerm"
+                    placeholder='Search Your Favourite Movie'
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { searchMovies(searchTerm) }}
+                />
+            </div>
+            {
+                movies.length > 0
+                ? (
+                    <div className="container">
+                      {
+                        movies.map(movie => (
+                            <MovieCard movie={movie} />
+                        ))
+                      }
+                    </div>  
+                 ) : (
+                    <div className='empty'>
+                        <h2>No movies Found</h2>
+                    </div>
+                 )
+            }
+
+            
+        </div>
+    )
+}
+
+export default App
